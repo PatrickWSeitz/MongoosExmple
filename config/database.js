a959c8fd79a5c407c80ae77ccbef1fdcf313e93b
PatrickWSeitz <pwseitz@email.neit.edu>
2023-02-02 14:15:31 -0500

    Week4

---
+++ config/database.js
@@ -0,0 +1,6 @@
+if(process.env.NODE_ENV === "production"){
+    module.exports = {mongoURI:"mongodb+srv://Pseitz:<dbPassword>@cluster0.jpeof3d.mongodb.net/?retryWrites=true&w=majority"}
+}
+else{
+    module.exports = {mongoURI:"mongodb://127.0.0.1:27017/gameEntries"}
+}
